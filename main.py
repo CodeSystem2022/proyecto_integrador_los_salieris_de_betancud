04890a9145b36312c42b87adb122952d6b1a1291
Leandro Gabriel Zalazar <legazarcia@gmail.com>
2023-07-05 19:51:54 -0300

    fix issue #18

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from routers import gastos
 
-app = FastAPI()
+description="""
+    API REST para la gestión de gastos
+    Proyecto integrador tercer semestre grupo Los salieries de betancud.
+    Consulte el endpoint /docs para la documentación Swagger y el endpoint /redoc para la
+    documentación ReDoc
+"""
+app = FastAPI(
+    title="Gestión de Gastos",
+    description=description
+)
+
+#routers (Se añade el router para gastos) 
+app.include_router(gastos.router)
 
 @app.get("/status", status_code=200)
 async def status():
